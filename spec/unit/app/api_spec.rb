55c76c44ef4080522020894c324efa391c12766c
cspmosh <cs.pmosh@gmail.com>
2020-09-24 14:14:22 -0500

    added tests for get 'expenses/:date' api which mock the ledger's response given a valid date for a case where there are records returned and a case where none are returned.

--- spec/unit/app/api_spec.rb
+++ spec/unit/app/api_spec.rb
@@ -37,6 +37,7 @@ module ExpenseTracker
           expect(response['status']).to eq(200)
         end
       end
+
       context 'when the expense fails validation' do
         let(:expense) { { 'some' => 'data'} }
 
@@ -54,6 +55,46 @@ module ExpenseTracker
         it 'responds with a 422 (Unprocessable entity)' do 
           response = postExpense(expense)
           expect(response['status']).to eq(422)
+        end
+      end
+    end
+
+    describe 'GET /expenses/:date' do
+      context 'when expenses exist on the given date' do
+        before do
+          allow(ledger).to receive(:expenses_on)
+            .with('2020-09-30')
+            .and_return(['expense_1', 'expense_2'])
+        end
+
+        it 'returns the expense records as JSON' do
+          get '/expenses/2020-09-30'
+          parsed = JSON.parse(last_response.body)
+          expect(parsed).to eq(['expense_1', 'expense_2'])
+        end
+
+        it 'response with a 200 (OK)' do
+          get '/expenses/2020-09-30'
+          expect(last_response.status).to eq(200)
+        end
+      end
+      
+      context 'when there are no expenses on the given date' do
+        before do
+          allow(ledger).to receive(:expenses_on)
+            .with('2020-09-32')
+            .and_return([])
+        end
+
+        it 'returns an empty array as JSON' do
+          get 'expenses/2020-09-32'
+          parsed = JSON.parse(last_response.body)
+          expect(parsed).to eq([])
+        end
+
+        it 'response with a 200 (OK)' do
+          get 'expenses/2020-09-32'
+          expect(last_response.status).to eq(200)
         end
       end
     end
